4f00857b22bef437b0e79f0258d033b7999080ba
hermacle <kihenyeghohermacle@gmail.com>
2024-03-04 16:28:33 +0100

    fix : icon display

--- src/App.jsx
+++ src/App.jsx
@@ -140,7 +140,8 @@ function App() {
         {/* Submit Button */}
         <div className="submit-bar">
           <button id="submit" className="submit-arrow" onClick={calculateAge}>
-            <img id="arrow-icon" src="src/assets/images/icon-arrow.svg" alt="arrow icon" />
+            {/* <img id="arrow-icon" src="src/assets/images/icon-arrow.svg" alt="arrow icon" /> */}
+            <svg xmlns="http://www.w3.org/2000/svg" id="arrow-icon" width="46" height="44" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" stroke-width="2"><path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/></g></svg>
           </button>
           <div className="bar"></div>
         </div>
